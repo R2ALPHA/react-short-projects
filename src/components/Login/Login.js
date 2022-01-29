43299d1b040aff077b3a7a8a67aa966100788598
gourav anand <gourav.07anand@gmail.com>
2022-01-29 12:25:07 +0530

    build and use custom context provider component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
 
@@ -40,6 +41,9 @@ const Login = (props) => {
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = emailState;
+
+  const authContext = useContext(AuthContext);
+
   useEffect(() => {
 
     const identifier = setTimeout(() => {
@@ -84,8 +88,9 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authContext.onLogin(emailState.value, passwordState.value);
   };
+
 
   return (
     <Card className={classes.login}>
