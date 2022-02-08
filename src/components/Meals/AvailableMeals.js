43f04aa18adfd68ca848c94f897de4ffa5cd6591
gourav anand <gourav.07anand@gmail.com>
2022-02-08 09:46:00 +0530

    add checkout form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
 
     const fetchMeals = async () => {
-      const response = await fetch('https://react-http-de395-default-rtdb.firebaseio.com/meals.djson');
+      const response = await fetch('https://react-http-de395-default-rtdb.firebaseio.com/meals.json');
 
       if (!response.ok) {
         throw new Error('Something went wrong');
