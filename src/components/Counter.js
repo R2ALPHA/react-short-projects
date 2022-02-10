2551c5b082db15008745b964cc124f415ad5f412
gourav anand <gourav.07anand@gmail.com>
2022-02-10 09:10:31 +0530

    use redux data in react hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux';  // We can use useStore too 
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const counter = useSelector(state => state.counter);  // Subscription set remove p
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
