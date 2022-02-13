43b798ee549d5b1f2400b6f3451cf4dd168b3015
gourav anand <gourav.07anand@gmail.com>
2022-02-13 11:09:58 +0530

    finalize the fetch logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -14,7 +14,10 @@ export const fetchCartData = () => {
         };
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity
+            }));
         } catch (error) {
             dispatch(uiActions.showNotifications({
                 status: 'error',
