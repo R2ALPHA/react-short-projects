b4e8337ccfec953cf3fcd48165a5ec4200b7815f
gourav anand <gourav.07anand@gmail.com>
2022-02-14 09:51:05 +0530

    add dummy data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,15 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is Fun' },
+    { id: 'q2', author: 'Gourav', text: 'Learning React is Very Fun' },
+    { id: 'q3', author: 'Max', text: 'Learning React is Extreme Fun' },
+    { id: 'q4', author: 'AnotherMax', text: 'Learning React is most Fun' },
+    { id: 'q5', author: 'NopsMax', text: 'Learning React is another level fun Fun' },
+];
+
 const AllQuotes = () => {
-    return <h1>All Quote</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuotes;
