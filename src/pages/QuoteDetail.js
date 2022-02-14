02625450af90c8d7380349d5f7b18dab5399f3a0
gourav anand <gourav.07anand@gmail.com>
2022-02-14 10:01:37 +0530

    output data on the details page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,27 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router";
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is Fun' },
+    { id: 'q2', author: 'Gourav', text: 'Learning React is Very Fun' },
+    { id: 'q3', author: 'Max', text: 'Learning React is Extreme Fun' },
+    { id: 'q4', author: 'AnotherMax', text: 'Learning React is most Fun' },
+    { id: 'q5', author: 'NopsMax', text: 'Learning React is another level fun Fun' },
+];
 
 const QuoteDetail = () => {
 
     const params = useParams();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+    if (!quote) {
+        return <p>No Quote Found !</p>;
+    }
 
     return <Fragment>
-        <h1>Quote detail</h1>
-        <p>{params.quoteId}</p>
+        <HighlightedQuote text={quote.text} author={quote.author}></HighlightedQuote>
         <Route path={`/quotes/${params.quoteId}/comments`}>
             <Comments />
         </Route>
