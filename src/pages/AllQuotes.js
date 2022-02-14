b3b7f68901a87fd98dd68302d159ec5553a8cec7
gourav anand <gourav.07anand@gmail.com>
2022-02-14 08:55:42 +0530

    add pages

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,5 @@
+const AllQuotes = () => {
+    return <h1>All Quote</h1>
+};
+
+export default AllQuotes;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuoteDetail = () => {
+    return <h1>Quote detail</h1>
+};
+
+export default QuoteDetail;
