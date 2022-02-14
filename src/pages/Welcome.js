612d2426e0646be67dfc9388a9d18fd0bd549956
gourav anand <gourav.07anand@gmail.com>
2022-02-14 08:30:10 +0530

    redirect to user

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+import { Route } from "react-router";
+
 const Welcome = () => {
-    return <h1>Welcome Page</h1>
+    return (
+        <Fragment>
+            <h1>The Welcome Page</h1>
+            <Route path="/welcome/new-user">
+                <p>Welcome, new user</p>
+            </Route>
+        </Fragment>
+    );
 };
 
 export default Welcome;
