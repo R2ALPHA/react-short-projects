cfb47e880ec72772aebf2d2fc7c0a8f9da2d15ff
gourav anand <gourav.07anand@gmail.com>
2022-02-16 10:27:44 +0530

    add authentication

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,97 @@
+import { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import AuthContext from '../../store/auth-context';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const history = useHistory();
+
+  const authCtx = useContext(AuthContext);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const eneteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // Optional: Add validation
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCqtZZQopsfYCVpsOXHU59_maAwYdvwUgk'
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCqtZZQopsfYCVpsOXHU59_maAwYdvwUgk';
+    }
+
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: eneteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+        // ...
+      } else {
+        res.json().then(data => {
+          let errorMessage = data?.error?.message ? data.error.message : 'Authentication failed !';
+          throw new Error(errorMessage);
+        });
+      }
+    }).then(data => {
+      authCtx.login(data.idToken);
+      history.replace('/');
+    }).catch(error => {
+      alert(error.message);
+    })
+  };
+
+  return (
+
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler} >
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input ref={emailInputRef} type='email' id='email' required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input ref={passwordInputRef} type='password' id='password' required />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p> Loading ...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
---
+++ src/components/Profile/ProfileForm.js
@@ -0,0 +1,45 @@
+import { useContext, useRef } from 'react';
+import AuthContext from '../../store/auth-context';
+import classes from './ProfileForm.module.css';
+
+const ProfileForm = () => {
+
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation 
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCqtZZQopsfYCVpsOXHU59_maAwYdvwUgk';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      // assumption: Always succeeds!
+
+    })
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input ref={newPasswordInputRef} type='password' minLength="7" id='new-password' />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
